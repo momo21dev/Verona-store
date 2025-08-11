f658ae0fb87650281a0ffafc0d6344a69f060bf5
mohamed mostafa <mohamed.mostafa4211@gmail.com>
2025-08-11 06:45:44 +0300

    0

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import { useState } from 'react'
+
 
 import './App.css'
 import Landing from './components/Landing'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
